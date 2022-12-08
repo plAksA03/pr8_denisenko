c0736824671032b123b3794201527de0d10457cb
Плаксевичь Михаил <plaksa_03@bk.ru>
2022-12-08 11:44:20 +0300

    Добавлен файл main.py цифры

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import random
 
 def password():
     a = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    c = ['1','2','3','4','5','6','7','8','9','0']
     otvet = ''
     for i in range(11):
-        otvet += random.choice(a)
+        otvet += random.choice(a + c)
     return otvet
 
 d = password()
