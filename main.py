bd043cda35d3c71cdea0e3e570ef0f723eba95c8
Плаксевичь Михаил <plaksa_03@bk.ru>
2022-12-08 11:35:30 +0300

    Добавлен файл main.py верхний регистр

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import random
 
 def password():
     a = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    b = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
     otvet = ''
     for i in range(11):
-        otvet += random.choice(a)
+        otvet += random.choice(a + b)
     return otvet
 
 d = password()
